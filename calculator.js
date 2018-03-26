1428338a14c88d1824f5aacb1100787dcc9a43dc
marietrull <marietrull@gmail.com>
2018-03-26 17:13:58 -0500

    commit 2 before checking out a new branch

--- calculator.js
+++ calculator.js
@@ -1,11 +1,8 @@
 console.log("I am building a calculator.");
 
-let numArray = [];
-let funcArray = [];
-
-//Display sequence in Output
-//$('header').text("Stored Numbers: " + numArray + "\n" + "Stored Functions: " + funcArray);
-
+let numArray = []; //Array for user selected numbers
+let funcArray = []; //Array for user selected functions
+let num2Array = []; //Array for computing more than two numbers
 
 //Click on number + store value
 
@@ -27,9 +24,12 @@ $('.functions').on('click', (e) => {
 	console.log(funcArray);
 })
 
-//Make equals button run top level math function
+//Make equals button run mid level math function
 $('.equals').on('click', (e) => {
 	math();
+	console.log(num2Array); 
+	console.log(numArray);
+	console.log(funcArray);
 })
 
 //Make clear button clear the arrays
@@ -44,13 +44,25 @@ $('.clear').on('click', (e) => {
 
 //top level math function
 
+// const compute = () => {
+// 	for(i = 0; i < numArray.length; i++){
+// 		math();
+// 	}	
+// }
+
+//mid level math function to compute last two numbers in array + last function in array
+
 const math = () => {
+		//Grab the second to last number in the array
 		let numOne = numArray[numArray.length - 2];
 		console.log("First Number: " + numOne);
+		//Grab the last number in the array
 		let numTwo = numArray[numArray.length - 1];
 		console.log("Second Number: " + numTwo);
+		// Grab the last function in the array
 		let funcOne = funcArray[funcArray.length - 1];
 		console.log("First Function: " + funcOne);
+		// Calculate based on last two numbers + last function
 		if(funcOne === 'x'){
 			multiply(numOne, numTwo);
 		} else if (funcOne === '/'){
@@ -59,28 +71,35 @@ const math = () => {
 			add(numOne, numTwo);
 		} else if (funcOne === '-'){
 			subtract(numOne, numTwo);
-		} else
-			console.log('Error');
+		}
+		//Remove used elements from arrays
+		numArray.pop(numOne);
+		numArray.pop(numTwo);
+		funcArray.pop(funcOne);
 };
 
-//sub math functions
+//bottom level math functions
 const multiply = (numOne, numTwo) => {
 	let productMe = numOne * numTwo;
+	num2Array.push(productMe);
 	$('header').text(numOne + " x " + numTwo + " = " + productMe);
 }
 
 const divide = (numOne, numTwo) => {
 	let divideMe = numOne / numTwo;
+	num2Array.push(divideMe);
 	$('header').text(numOne + " / " + numTwo + " = " + divideMe);
 }
 
 const add = (numOne, numTwo) => {
 	let addMe = numOne + numTwo;
+	num2Array.push(addMe);
 	$('header').text(numOne + " + " + numTwo + " = " + addMe);
 }
 
 const subtract = (numOne, numTwo) => {
 	let subtractMe = numOne - numTwo;
+	num2Array.push(subtractMe);
 	$('header').text(numOne + " - " + numTwo + " = " + subtractMe);
 }
 
