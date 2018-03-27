7d7565e73c677da2530f6df95cce8c3756553206
Zoe <zoereagan@gmail.com>
2018-03-26 22:40:27 -0500

    completed number, operator and clear button functionality; equals still does not function

--- js/app.js
+++ js/app.js
@@ -17,28 +17,54 @@ let $operators = $('.operations');
 let $equals = $('#equals');
 let $clear = $('#clear');
 
+//these will be used in the final expression
+let intOne = [];
+let intTwo = [];
 
 //define functions for clicking on numbers, operators & clear/equals IDs
 $numbers.on('click', () => {
   //get value of number
   numValue = $(event.currentTarget).text()
-  console.log(numValue);
+    console.log(numValue);
+  //store number displayed
+  numToDisplay = $display.text($display.text() + numValue).text();
 });
 
 $operators.on('click', () => {
-  //get value of number
+  //get value of number clicked
   opsValue = $(event.currentTarget).text()
   console.log(opsValue);
+	//store operations displayed
+	opToDisplay = $display.text($display.text() + opsValue).text();
+	// }
 });
 
 $equals.on('click', () => {
-  //get value of number
+  //get value of numbers and operators
+  //here is where your math logic lives!
   eqValue = $(event.currentTarget).text()
   console.log(eqValue);
+  if(opToDisplay === '+') {
+    numValue + numToDisplay
+  } else if(opToDisplay === '-') {
+    numValue - numToDisplay
+  } else if (opToDisplay === '*') {
+    numValue * numToDisplay
+  } else if (opToDisplay === '/') {
+    numValue / numToDisplay
+  } else {
+    null;
+  }
+  
+  //still needs functionality
 });
 
 $clear.on('click', () => {
-  //get value of number
-  clearValue= $(event.currentTarget).text()
+  clearValue = $(event.currentTarget).text()
   console.log(clearValue);
+//if all data = null, clear the display
+  intOne = null;
+	$operators = null;
+	intTwo = null;
+	$display.empty()
 });
